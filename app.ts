d74b4a8da196de25685efc0be027e191231a88c8
Stan Hamara <stanislav.hamara@gmail.com>
2023-09-05 09:43:15 +0100

    Parse turns and distances from input data

--- app.ts
+++ app.ts
@@ -1,3 +1,23 @@
-import { directions } from "./data";
+import { data } from "./data";
+import { Coordinates, Direction } from "./types";
 
-console.log({ directions })
+const parseDirections = (directions: string[]) => {
+
+    let currentDirection = Direction.N
+    let currentCoordinates: Coordinates = { x: 0, y: 0 }
+
+    directions.forEach(direction => {
+        console.log({ direction })
+        const turnDirection = direction[0]
+        const distance = parseInt(direction.slice(1))
+        if ((turnDirection !== 'R' && turnDirection !== 'L')) {
+            throw Error(`Input data is invalid. "${turnDirection}" is not a valid turn direction`)
+        } else if ( isNaN(distance)) {
+            throw Error(`Input data is invalid. "${direction}" does not contain numeric distance value`)
+        }
+
+        console.log({ direction })
+    })
+}
+
+parseDirections(data)
